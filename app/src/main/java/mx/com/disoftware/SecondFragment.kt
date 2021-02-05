8ed3b08379f2b9e26b35b80587ec6ad66251f7f3
Melchiah Razziel Reaven <razzielreaven@gmail.com>
2021-02-05 17:14:47 -0600

    Navegación entre fragmentos utilizando deep link.

--- app/src/main/java/mx/com/disoftware/SecondFragment.kt
+++ app/src/main/java/mx/com/disoftware/SecondFragment.kt
@@ -1,10 +1,13 @@
 package mx.com.disoftware
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavArgs
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
@@ -21,8 +24,27 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val textViewSecondFragment = view.findViewById<TextView>(R.id.text_view_second_fragment)
+        val buttonDecondFragment = view.findViewById<Button>(R.id.button_second_fragment)
+        val buttonGoSecondFragmentProductSecond = view.findViewById<Button>(R.id.button_go_second_fragment_product_second)
+
         textViewSecondFragment.text = "${nombre} ${edad}"
+
+        buttonDecondFragment.setOnClickListener {
+            // navegar al primer fragmento (principal, el que tiene la casita de product_second.xml)
+            // Sólo requiere de agregar la felcha a donde se desea navegar de main_graph.xml al product_graph.xml
+            // se navega al que esté marcado como principal (con la casita).
+            findNavController().navigate(R.id.product_graph)
+        }
+
+        buttonGoSecondFragmentProductSecond.setOnClickListener {
+            // Navegar directamente a un fragmento en específico (distinto al principal) de product_graph.xml
+            // 1. agregarlo como propiedad en la sección de deep link  en el product_fragment.xml dentro de la navigation product_graph.xml.
+            // 2. agregar el deep link en el manifest
+            findNavController().navigate(Uri.parse("deeplinkejem://card"))
+        }
+
     }
 
 }
